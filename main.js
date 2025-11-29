0b92ab3bd742181d5879477a5a43444769e79a30
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-11-29 23:33:05 +0600

    start method in packeg .json"

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+const express =require('express');
+
+
+const app = express();
+
+app.use(express.json())
+
+
+
+
+app.length=(5000 ,()=>{
+    console.log('server is runnig on port 5000')
+})
+
