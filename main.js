6f5a8f807a1fb1e54019421f2992f20bf579fefb
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-11-29 23:38:36 +0600

    server is runnig  nedd to connect database

--- main.js
+++ main.js
@@ -11,6 +11,27 @@ app.use(express.json())
 
 
 
+
+
+
+
+
+
+
+
+
+
+app.get('/' ,(req,res)=>{
+    res.send('hello rakib here')
+})
+
+
+
+
+
+
+
+
 app.listen(5000 ,()=>{
     console.log('server is running on port 5000')
 })
